f7860fd5a0b9165f3ee7260caedbd71b7a51d534
Timothy Oyetayo <toyetayo@academic.rrc.ca>
2024-07-22 23:36:51 -0500

    set up of orders

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,49 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @order = current_user.orders.build
+    @shopping_cart_items = current_user.shopping_cart_items.includes(:product)
+  end
+
+  def create
+    @order = current_user.orders.build(order_params)
+    @order.status = 'new'
+    if @order.save
+      process_order_items
+      current_user.shopping_cart_items.destroy_all
+      redirect_to @order, notice: 'Order placed successfully.'
+    else
+      @shopping_cart_items = current_user.shopping_cart_items.includes(:product)
+      render :new
+    end
+  end
+
+  def show
+    @order = current_user.orders.find(params[:id])
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:address, :province_id)
+  end
+
+  def process_order_items
+    current_user.shopping_cart_items.each do |cart_item|
+      @order.order_items.create!(
+        product: cart_item.product,
+        quantity: cart_item.quantity,
+        price: cart_item.product.price
+      )
+    end
+    calculate_total
+  end
+
+  def calculate_total
+    tax_rates = @order.province.slice(:pst_rate, :gst_rate, :hst_rate).values.compact
+    subtotal = @order.order_items.sum('quantity * price')
+    total_tax = tax_rates.sum { |rate| subtotal * rate }
+    @order.update(total: subtotal + total_tax)
+  end
+end
